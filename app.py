b8976257881b7b5a3deff3156edf4c61ed428147
Andreas <andreasagap@gmail.com>
2021-05-04 23:07:28 +0300

    Initial web api

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, url_for, redirect, render_template, request, abort
 
 import flask_admin
-from flask_admin import helpers as admin_helpers
+from flask_admin import helpers as admin_helpers, AdminIndexView
 from flask_admin import BaseView, expose
 
 # Create Flask application
@@ -11,47 +11,34 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 
-
-
-
-
-
-class CustomView(BaseView):
+# Flask views
+@app.route('/admin')
+def index():
+    return render_template('index.html',ratings_acropolis=50)
+class MyHomeView(AdminIndexView):
     @expose('/')
     def index(self):
-        return self.render('admin/custom_index.html')
-
-# Flask views
-@app.route('/')
-def index():
-    return render_template('index.html')
-
+        ratings_acropolis = '500'
+        return self.render('admin/index.html', ratings_acropolis=ratings_acropolis)
 # Create admin
+
 admin = flask_admin.Admin(
     app,
     'My Dashboard',
-    base_template='my_master.html',
+    base_template='admin/my_master.html',
     template_mode='bootstrap4',
+    index_view=MyHomeView(name='Home',url='/')
+
 )
-
 # Add model views
-admin.add_view(CustomView(name="Custom view", endpoint='custom', menu_icon_type='fa', menu_icon_value='fa-connectdevelop',))
+# #admin.add_view(CustomView(name="Custom view", endpoint='custom', menu_icon_type='fa', menu_icon_value='fa-connectdevelop',))
 
-# define a context processor for merging flask-admin's template context into the
-# flask-security views.
-def security_context_processor():
-    return dict(
-        admin_base_template=admin.base_template,
-        admin_view=admin.index_view,
-        h=admin_helpers,
-        get_url=url_for
-    )
+
 
 if __name__ == '__main__':
 
     # Build a sample db on the fly, if one does not exist yet.
     app_dir = os.path.realpath(os.path.dirname(__file__))
-    database_path = os.path.join(app_dir, app.config['DATABASE_FILE'])
 
 
     # Start app
